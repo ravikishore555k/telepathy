4e409930e494998bda89a6c291787a9fe086441a
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-08 04:19:39 +0800

    Update aws-vpc-networking.tf

--- aws-vpc-networking.tf
+++ aws-vpc-networking.tf
@@ -73,6 +73,6 @@ resource "aws_security_group" "telepathy-sg" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
+   # prefix_list_ids = ["pl-12c4e678"]
   }
 }
