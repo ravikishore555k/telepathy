3578580b0e429d2bb8b9477048beb0a139eb554f
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-08 01:34:23 +0800

    Update aws-vpc-networking.tf

--- aws-vpc-networking.tf
+++ aws-vpc-networking.tf
@@ -4,60 +4,60 @@ provider "aws" {
   region     = "ap-south-1"
 }
 
-resource "aws_vpc" "mysqldbvpc" {
+resource "aws_vpc" "telepathy-vpc" {
   cidr_block       = "${var.vpc-cidr}"
   instance_tenancy = "${var.tenancy}"
   tags = {
-    name = "mysqldbvpc-1"
+    name = "telepathy-vpc"
   }
 }
 
-resource "aws_subnet" "mysqldbsubnet" {
-  vpc_id            = "${aws_vpc.mysqldbvpc.id}"
+resource "aws_subnet" "telepathy-subnet" {
+  vpc_id            = "${aws_vpc.telepathy-vpc.id}"
   cidr_block        = "${var.subnet-cidr}"
   availability_zone = "ap-south-1"
   tags = {
-    name = "mysqldbsubnet-1"
+    name = "telepathy-subnet-1"
   }
 }
 
-resource "aws_route_table" "mysqldbrt" {
-  vpc_id = "${aws_vpc.mysqldbvpc.id}"
+resource "aws_route_table" "telepathy-rt" {
+  vpc_id = "${aws_vpc.telepathy-vpc.id}"
 
   route {
     cidr_block = "${var.route-block}"
-    gateway_id = "${aws_internet_gateway.mysqldb-igw.id}"
+    gateway_id = "${aws_internet_gateway.telepathy-igw.id}"
   }
   tags = {
-    name = "mysqldbrt"
+    name = "telepathy-rt"
   }
 }
 resource "aws_route_table_association" "a" {
-  subnet_id      = "${aws_subnet.mysqldbsubnet.id}"
-  route_table_id = "${aws_route_table.mysqldbrt.id}"
+  subnet_id      = "${aws_subnet.telepathy-subnet.id}"
+  route_table_id = "${aws_route_table.telepathy-rt.id}"
 }
 output "vpc_id" {
-  value = "${aws_vpc.mysqldbvpc.id}"
+  value = "${aws_vpc.telepathy-vpc.id}"
 }
 output "subnet_id" {
-  value = "${aws_subnet.mysqldbsubnet.id}"
+  value = "${aws_subnet.telepathy-subnet.id}"
 }
 #output "instance_ips" {
 #  value = ["${aws_instance.mysqldb.*.public_ip}"]
 #}
-resource "aws_internet_gateway" "mysqldb-igw" {
-  depends_on = ["aws_vpc.mysqldbvpc"]
-  vpc_id     = "${aws_vpc.mysqldbvpc.id}"
+resource "aws_internet_gateway" "telepathy-igw" {
+  depends_on = ["aws_vpc.telepathy-vpc"]
+  vpc_id     = "${aws_vpc.telepathy-vpc.id}"
 
   tags = {
-    name = "mysqldb-igw"
+    name = "telepathy-igw"
   }
 }
 #newcode
-resource "aws_security_group" "mysqldbsg" {
- name        = "mysqldbsg"
+resource "aws_security_group" "telepathy-sg" {
+ name        = "telepathy-sg"
   #description = "Allow TLS inbound traffic"
-  vpc_id      = "${aws_vpc.mysqldbvpc.id}"
+  vpc_id      = "${aws_vpc.telepathy-vpc.id}"
   
 
   ingress {
