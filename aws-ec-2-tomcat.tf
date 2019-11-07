1b9ba80ee4005555fba2b5b5a08f5c57c8fa3d1f
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-08 01:01:22 +0800

    Create aws-ec-2-tomcat.tf

---
+++ aws-ec-2-tomcat.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "mysqldb" {
+  #depends_on = ["aws_internet_gateway.mysqldb-igw"]
+  depends_on                  = ["aws_subnet.mysqldbsubnet"]
+  count                       = "${var.ec2-count}"
+  ami                         = "${var.ami-id}"
+  instance_type               = "${var.instance-type}"
+  subnet_id                   = "${aws_subnet.mysqldbsubnet.id}"
+  associate_public_ip_address = "true"
+  enable_dns_hostnames = "enable"
+  #vpc_security_group_ids = ["sg-04d8b0915426482ed"]
+  #vpc_security_group_ids = ["${var.security-groupid}"]
+  vpc_security_group_ids = ["${aws_security_group.mysqldbsg.id}"]
+  #"sg-04d8b0915426482ed"
+  key_name                    = "lalu"
+  user_data                   = "${file("install-mysql.sh")}"
+  tags = {
+    #    name = "mysql-db-${count.index + 1}"
+    name = "mysqldb-1"
+  }
+}
