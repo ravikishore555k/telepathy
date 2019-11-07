7dc6a806c431631f87ba00ebb471fda6b8465089
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-08 01:58:21 +0800

    Update aws-variables.tf

--- aws-variables.tf
+++ aws-variables.tf
@@ -2,7 +2,7 @@ variable "ec2-count" {
   default = "1"
 }
 variable "ami-id" {
-  default = "ami-07b4f3c02c7f83d59"
+  default = "ami-0123b531fc646552f"
 }
 variable "instance-type" {
   default = "t2.micro"
@@ -10,9 +10,9 @@ variable "instance-type" {
 #variable "subnet-id" {
 #       default="${subnet_id}"
 #}
-variable "aws-region" {
-  default = "us-west-2a"
-}
+#variable "aws-region" {
+ # default = "us-west-2a"
+#}
 
 #variable "instance-tag" {
 #  type    = list
