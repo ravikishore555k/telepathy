65856272cebba0ae54d75ff7b91a5fd321119135
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-09 00:04:00 +0800

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
-
+sudo apt install software-properties-common apt-transport-https -y
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt install openjdk-8-jdk -y
