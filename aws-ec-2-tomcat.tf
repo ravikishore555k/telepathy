b9fbdb3c6a616fa49722e6512d210750cead6490
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-08 14:40:38 +0800

    Update aws-ec-2-tomcat.tf

--- aws-ec-2-tomcat.tf
+++ aws-ec-2-tomcat.tf
@@ -5,8 +5,8 @@ resource "aws_instance" "tomcat" {
   ami                         = "${var.ami-id}"
   instance_type               = "${var.instance-type}"
   subnet_id                   = "${aws_subnet.telepathy-subnet.id}"
-  #associate_public_ip_address = "true"
-  #enable_dns_hostnames = "enable"
+  associate_public_ip_address = "true"
+  enable_dns_hostnames = "enable"
   #vpc_security_group_ids = ["sg-04d8b0915426482ed"]
   #vpc_security_group_ids = ["${var.security-groupid}"]
   vpc_security_group_ids = ["${aws_security_group.telepathy-sg.id}"]
