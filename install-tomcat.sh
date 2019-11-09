b9760a33664781183791849c5bd2a2beaa63c76b
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-10 02:22:47 +0800

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -4,3 +4,10 @@ sudo apt-get upgrade -y
 sudo apt install software-properties-common apt-transport-https -y
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt install openjdk-8-jdk -y
+sudo su
+echo PermitRootLogin yes >> /etc/ssh/sshd_config
+service ssh restart
+
+
+
+
