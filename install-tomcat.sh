9bdd40fbad0561592b937bb71b6bc4a82d356371
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-10 13:43:56 +0800

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -4,6 +4,8 @@ sudo apt-get upgrade -y
 sudo apt install software-properties-common apt-transport-https -y
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt install openjdk-8-jdk -y
+sudo ufw allow from any to any port 22 proto tcp
+sudo chown ubuntu:ubuntu /opt/
 sudo su
 echo PermitRootLogin yes >> /etc/ssh/sshd_config
 service ssh restart
