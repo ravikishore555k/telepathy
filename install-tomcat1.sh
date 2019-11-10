6afaba18bc04b17aa1435bc92868fdb0ff32c4af
ravikishore555k <39574642+ravikishore555k@users.noreply.github.com>
2019-11-11 04:32:58 +0800

    Update install-tomcat1.sh

--- install-tomcat1.sh
+++ install-tomcat1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt install software-properties-common apt-transport-https -y
-sudo add-apt-repository ppa:openjdk-r/ppa -y
-sudo apt install openjdk-8-jdk -y
+##sudo apt-get upgrade -y
+#sudo apt install software-properties-common apt-transport-https -y
+#sudo add-apt-repository ppa:openjdk-r/ppa -y
+#sudo apt install openjdk-8-jdk -y
 sudo ufw allow from any to any port 22 proto tcp
 sudo chown ubuntu:ubuntu /opt/
 sudo su
